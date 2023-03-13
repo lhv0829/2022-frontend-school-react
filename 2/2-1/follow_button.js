7635a1da61ac842686c4abcf79d31aac95f99166
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2023-03-14 03:27:58 +0900

    실습

--- 2/2-1/follow_button.js
+++ 2/2-1/follow_button.js
@@ -1,5 +1,39 @@
 // React Component!
 // Follow <-> following
 function FollowButton() {
-  return React.createElement("div", {}, "Follow");
+  const [following, setFollowing] = React.useState(false);
+
+  const commonBtnStyle = {
+    display: "flex",
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    top: '12px',
+    right: '16px',
+    width: '100px',
+    height: '36px',
+    borderRadius: '9999px',
+    fontWeight: 'bold',
+  };
+
+  const followBtnStyle = {
+    ...commonBtnStyle,
+    backgroundColor: 'black',
+    color: 'white',
+  }
+
+  const followingBtenStyle = {
+    ...commonBtnStyle,
+    backgroundColor: 'white',
+    color: 'black',
+    border: '1px solid #cfd9de',
+  };
+
+  return React.createElement("div", {
+    onClick: () => setFollowing(!following),
+    style: following ? followingBtenStyle : followBtnStyle,
+  }, following ? "following" : "Follow");
 }
+
+const domContainer = document.querySelector('#follow_button_container');
+ReactDOM.render(React.createElement(FollowButton), domContainer);
